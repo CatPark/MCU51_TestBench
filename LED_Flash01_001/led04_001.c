3554b07fde5e1b4afbe1cf148d23e8b51713de21
Cathy Du <catdx7@gmail.com>
2012-03-11 01:25:39 +0800

    first commit

---
+++ LED_Flash01_001/led04_001.c
@@ -0,0 +1,54 @@
+#include <REGX52.H>
+
+void delay();
+
+void main()
+{
+	for(;;)
+	{
+		P0=0xfe;
+		delay();
+		P0=0xfd;
+		delay();
+		P0=0xfb;
+		delay();
+		P0=0xf7;
+		delay();
+
+		P0=0xef;
+		delay();
+		P0=0xdf;
+		delay();
+		P0=0xbf;
+		delay();
+		P0=0x7f;
+		delay();
+		P0=0xff;
+		delay();
+
+		P1=0x7f;
+		delay();
+		P1=0xbf;
+		delay();
+		P1=0xdf;
+		delay();
+		P1=0xef;
+		delay();
+
+		P1=0xf7;
+		delay();
+		P1=0xfb;
+		delay();
+		P1=0xfd;
+		delay();
+		P1=0xfe;
+		delay();
+		P1=0xff;
+		delay();
+	}
+}
+void delay()
+{
+	int n;
+	for(n=0;n<=30000;n++);
+}
