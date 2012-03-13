1a354b87bdc25f01bab6ce3c1c80bbf9e80ed1f7
Cathy Du <catdx7@gmail.com>
2012-03-14 00:23:55 +0800

    This is another test

--- LED_Flash01_001/led04_001.c
+++ LED_Flash01_001/led04_001.c
@@ -1,7 +1,7 @@
 #include <REGX52.H>
 
 /* Git test 01 - add this line for Git testing */
-/* Git test 01 - add this line for Git testing */
+/* Git test 01 - add this line for Git testing again #2 */
 
 void delay();
 
