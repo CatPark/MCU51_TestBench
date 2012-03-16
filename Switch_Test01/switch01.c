d13fc3068273a6742b0e8d8479ed3022b57171dd
Cathy Du <catdx7@gmail.com>
2012-03-17 04:11:25 +0800

    Reverse the status of switch and LEDs

--- Switch_Test01/switch01.c
+++ Switch_Test01/switch01.c
@@ -4,7 +4,7 @@ main()
 {
  unsigned char port_in, key;
 
- P0=0x00;		/* Turn off all P0 LEDs */
+ P0=0xFF;		/* Turn off all P0 LEDs */
 
  P1=0xF0;		/* Prepare to get standalone switches K5 to K8 */
 
@@ -12,7 +12,7 @@ main()
  {
   port_in = P1;					/* Read P1 to detect switch status */
 
-  key = P1 & 0x80;				/* Check K8 status (check bit7 of Port1) */
+  key = ~P1 & 0x80;				/* Check K8 status (check bit7 of Port1) */
 
   if (key != 0) P0=0xFF;		/* K8 pressed, turn LEDs on  */
   else P0=0;					/* K8 not pressed,   LED off */	
