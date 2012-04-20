4571580e7cd7a2544d50554907a88220e559e8bc
Cathy Du <catdx7@gmail.com>
2012-04-21 03:36:00 +0800

    add Timer and ISR: Interrupt Service Routine

--- BEEP_Test01/beep_test01.c
+++ BEEP_Test01/beep_test01.c
@@ -22,21 +22,40 @@ sbit L00=P0^0;
 void beep01(void)
 {
 	unsigned int j;
-	while(1)
-	{
-		L00=0;
+		// L00=0;
 		for(j=1000;j>0;j--)
 		{
 			BEEP=~BEEP;
 			delayst(2);
 		}
-		L00=1;
+		// L00=1;
 		BEEP=1;
 		delay(500);
-	}
+
+}
+
+void InitTimer0(void)
+{
+	TR0  = 0;			// Stop Timer
+	TF0	 = 0;			// Reset Overflow
+    TMOD = 0x01; 		// Set Timer0 to Mode 1 (1 shot)
+    TH0 = 0x00; 		// Delay time counter delay about 65536us=66ms
+    TL0 = 0x01;
+    EA = 1;
+    ET0 = 1;
+    TR0 = 1;	  		// Start Timer
+}
+
+void ISR_Timer0(void) interrupt 1 using 2	// Timer Interrupt Service Routine
+{
+   P0 = ~P0;
+   InitTimer0();	   // Reste Timer
 }
 
 void main()
 {
-  beep01();
+  P0 = 0x00;
+  InitTimer0();			// Start Timer
+
+  for(;;) beep01();
 }
