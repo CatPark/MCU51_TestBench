93f9cff21540d94e5204742dc7b276438334de18
Cathy Du <catdx7@gmail.com>
2012-04-25 00:13:31 +0800

    Work 001

--- EPiano001/EP001_Core.c
+++ EPiano001/EP001_Core.c
@@ -1,27 +1,45 @@
 #include <STC89.H>
 
-#define	SPEAKER = P3.6
-
-
-
-
-
-
-
-
-void main()
+unsigned checkKey()
 {
- int freq[]=1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000;
+ unsigned a;
+ unsigned b;
 
- unsigned char n;
+ P1 = 0xF0;
+ P2 = 0x3C;
+
+ a = ~P1;
+ b = ~P3;
+
+ a = a & 0xF0;
+ b = b & 0x3C;
+
+ if (b == 0x04) return (0);
+ else if (b == 0x08) return (1);
+ else if (b == 0x10) return (2);
+ else if (b == 0x20) return (3);
+ else if (a == 0x10) return (4);
+ else if (a == 0x20) return (5);
+ else if (a == 0x40) return (6);
+ else if (a == 0x80) return (7);
+ else return (33);
+}
+
+sbit BP = P3^6;
+
+void main(void)
+{
+ unsigned int delay[]={10, 20, 30, 40, 50, 60, 70, 80};
+ unsigned int d;
+ unsigned char key;
 
  for (;;)
  {
-  while ( (n = checkKey()) > 7);
+  while ((key = checkKey()) > 7);
 
-  for (d=0; d < freq[n]; d++);
-  SPEAKER = ~SPEAKER;
-  for (d=0; d < freq[n]; d++);
-  SPEAKER = ~SPEAKER;
+  for (d=0; d<delay[key]; d++);
+  BP = ~BP;
+  for (d=0; d<delay[key]; d++);
+  BP = ~BP;
  }
 }
