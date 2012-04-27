603b8a58ea67f4bbf0ed7d3ba0c07c4cc2d9d058
Michael <michsin88@yahoo.com>
2012-04-27 16:52:01 +0800

    Make it portable between RichMCU & XT100

--- Sw001/Sw001.c
+++ Sw001/Sw001.c
@@ -1,31 +1,59 @@
 #include <STC89.H>
 
-char led( char a);
+/*******************************************
+   Set conditional compiling as below:
+   RICHMCU - for RichMCU Development Board
+   XT100   - for XT100   Development Board
+********************************************/
+#define		RICHMCU
 
-void main(void)
+#ifdef		RICHMCU
+#define		PORT_LED	P0
+#define		PORT_SW		P1
+#define		PORT_MSK	0xF0
+#define		SW1			0x70
+#define		SW2			0xB0
+#define		SW3	  		0xD0
+#define		SW4			0xE0
+#define		PTN1		0xC0
+#define		PTN2		0x30
+#define		PTN3		0x0C
+#define		PTN4		0x03
+#endif
 
-{
-	unsigned int x;
-			
- 	P1=0xFF;
-	for(;;)
-	{
-	 	x=P1;
-		x=x & 0xF0;
+#ifdef		XT100
+#define		PORT_LED	P0
+#define		PORT_SW		P2
+#define		PORT_MSK	0x0F
+#define		SW1			0x0E
+#define		SW2			0x0D
+#define		SW3	    	0x0B
+#define		SW4			0x07
+#define		PTN1		0x03
+#define		PTN2		0x0C
+#define		PTN3		0x30
+#define		PTN4		0xC0
+#endif
 
-		if  (x==0x70) P0=led(0xC0);
-		else if(x==0xB0) P0=led(0x30);
-		else if(x==0xD0) P0=led(0x0C);
-		else if(x==0xE0) P0=led(0x03);
-		else P0=led(0x00);
-	}
-
-}
-char led( char a)
+char led(char a)
 {
 	return ~a;	
 }
 
+void main(void)
+{
+	unsigned int x;
+			
+ 	PORT_SW=0xFF;
+	for(;;)
+	{
+	 	x=PORT_SW;
+		x=x & PORT_MSK;
 
-
-		  
+		if (x==SW1) PORT_LED=led(PTN1);
+		else if(x==SW2) PORT_LED=led(PTN2);
+		else if(x==SW3) PORT_LED=led(PTN3);
+		else if(x==SW4) PORT_LED=led(PTN4);
+		else PORT_LED=led(0x00);
+	}
+}  
